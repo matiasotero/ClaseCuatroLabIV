9d3d867678ea87fb9eb187cbd938c59859a12b4c
matiasotero <matiotero_1990@hotmail.com>
2017-04-20 22:10:41 -0300

    segunda parte de la clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,10 @@ export class AppComponent {
   title = 'app works!';
   nombre: string = "https://www.google.com.ar";
   mostrar:boolean = false;
+  dato:number = 0;
+  ruta:string="/assets/Koala.jpg";
+  tamanio:number=5;
+  posicion:string="absolute";
 
   MostrarNombre(){
     console.log(this.nombre);
